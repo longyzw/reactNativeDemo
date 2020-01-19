a9a82db53619cfec6abbb34f976a2eb69b7bcd12
longyzw <752490640@qq.com>
2020-01-19 23:08:25 +0800

    去除运行的黄色警告

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ import {AppRegistry} from 'react-native';
 import { name as appName } from './app.json';
 import App from './view/App'; // 开发入口
 
+// 关闭手机上全部黄色警告
+console.ignoredYellowBox = ['Warning: BackAndroid is deprecated. Please use BackHandler instead.', 'source.uri should not be an empty string', 'Invalid props.style key'];
+console.disableYellowBox = true
+
 AppRegistry.registerComponent(appName, () => App );
